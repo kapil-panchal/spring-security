2dded8a0e90ad16b799d0722d202edc9708a761e
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-10-17 10:53:01 +0530

    [refactor]

---
+++ easyschool-1/src/main/java/com/java/school/Easyschool1Application.java
@@ -0,0 +1,13 @@
+package com.java.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easyschool1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easyschool1Application.class, args);
+	}
+
+}
