9b0fdcee877ce3a4d1709bd9c8bf2621a9d201cd
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-10-18 09:42:15 +0530

    [model]

--- spring-security-8/src/main/java/com/java/security/model/Customer.java
+++ spring-security-8/src/main/java/com/java/security/model/Customer.java
@@ -1,26 +1,39 @@
 package com.java.security.model;
 
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "customer")
 @Data
 public class Customer {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String email;
-	private String pwd;
-	
-	@Column(name = "role")
-	private String role;
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String pwd;
+
+    private String role;
+
+    @Column(name = "create_dt")
+    @JsonIgnore
+    private Date createDt;
 }
