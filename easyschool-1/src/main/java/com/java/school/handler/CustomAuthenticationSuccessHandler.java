a3b67547cd898f7d96f2386404d744564c29f643
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-10-18 08:13:22 +0530

    [security context] added UserDetails logs

--- easyschool-1/src/main/java/com/java/school/handler/CustomAuthenticationSuccessHandler.java
+++ easyschool-1/src/main/java/com/java/school/handler/CustomAuthenticationSuccessHandler.java
@@ -3,6 +3,7 @@ package com.java.school.handler;
 import java.io.IOException;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -18,8 +19,15 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 		log.info("Login successful for user: {}", authentication.getName());
+		log.info("Login successful for username: {}", userDetails.getUsername());
+		log.info("Login successful for password: {}", userDetails.getPassword());
+		log.info("Login successful for getAutorities: {}", userDetails.getAuthorities());
+		log.info("Login successful for isAccountNonExpired: {}", userDetails.isAccountNonExpired());
+		log.info("Login successful for isAccountNonLocked: {}", userDetails.isAccountNonLocked());
+		log.info("Login successful for isCredentialsNonExpired: {}", userDetails.isCredentialsNonExpired());
+		log.info("Login successful for isEnabled: {}", userDetails.isEnabled());
 		response.sendRedirect("/dashboard");
 	}
 }
