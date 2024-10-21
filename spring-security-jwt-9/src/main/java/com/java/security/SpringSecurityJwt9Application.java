06d95931ad6a32c9ca3dcb7aaefaa940baef9fd8
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-10-21 13:00:35 +0530

    [refactor]

---
+++ spring-security-jwt-9/src/main/java/com/java/security/SpringSecurityJwt9Application.java
@@ -0,0 +1,13 @@
+package com.java.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwt9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwt9Application.class, args);
+	}
+
+}
