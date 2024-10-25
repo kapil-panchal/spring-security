71ad5229d35881d2ebe9bc50362e944fa55a3621
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2024-10-25 12:55:31 +0530

    [refactor]

---
+++ spring-security-method-security-10/src/main/java/com/java/security/SpringSecurityMethodSecurity10Application.java
@@ -0,0 +1,13 @@
+package com.java.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMethodSecurity10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMethodSecurity10Application.class, args);
+	}
+
+}
---
+++ spring-security-method-security-10/src/test/java/com/java/security/SpringSecurityMethodSecurity10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMethodSecurity10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
