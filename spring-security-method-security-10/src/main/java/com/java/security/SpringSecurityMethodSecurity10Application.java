745555d9ed3c7b051448450401baa5eb902f418e
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2024-10-25 19:40:38 +0530

    [method] method level security

--- spring-security-method-security-10/src/main/java/com/java/security/SpringSecurityMethodSecurity10Application.java
+++ spring-security-method-security-10/src/main/java/com/java/security/SpringSecurityMethodSecurity10Application.java
@@ -2,12 +2,15 @@ package com.java.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class SpringSecurityMethodSecurity10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityMethodSecurity10Application.class, args);
 	}
-
 }
