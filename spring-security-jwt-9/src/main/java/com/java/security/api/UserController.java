ee4bae85ec34831a52a91405c4293692da542eb7
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2024-10-25 11:01:32 +0530

    [record class]

--- spring-security-jwt-9/src/main/java/com/java/security/api/UserController.java
+++ spring-security-jwt-9/src/main/java/com/java/security/api/UserController.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.security.model.Customer;
+import com.java.security.model.LoginRequestDTO;
+import com.java.security.model.LoginResponseDTO;
 import com.java.security.repository.CustomerRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -45,10 +47,15 @@ public class UserController {
         }
     }
 
-    @RequestMapping("/user")
+    @GetMapping("/user")
     public Customer getUserDetailsAfterLogin(Authentication authentication) {
         Optional<Customer> optionalCustomer = customerRepository.findByEmail(authentication.getName());
         return optionalCustomer.orElse(null);
+    }
+    
+    @PostMapping(path = "/apiLogin")
+    public ResponseEntity<LoginResponseDTO> apiLogin(@RequestBody LoginRequestDTO loginRequest){
+    	
     }
 
 }
