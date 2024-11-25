af5e8e9b3ead2ecdc02f09038d65c310c96c3f3e
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-11-25 11:12:55 +0530

    [auth server] spring auth server

---
+++ spring-security-auth-server-12/src/main/java/com/java/security/SpringSecurityAuthServer12Application.java
@@ -0,0 +1,13 @@
+package com.java.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthServer12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthServer12Application.class, args);
+	}
+
+}
