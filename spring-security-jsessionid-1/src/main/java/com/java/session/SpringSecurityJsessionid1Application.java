5adb91d728f1edbc14c83633577f6f1ae6efdca4
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2025-06-17 06:52:20 +0530

    [init]

---
+++ spring-security-jsessionid-1/src/main/java/com/java/session/SpringSecurityJsessionid1Application.java
@@ -0,0 +1,13 @@
+package com.java.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJsessionid1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJsessionid1Application.class, args);
+	}
+
+}
---
+++ spring-security-jsessionid-1/src/test/java/com/java/session/SpringSecurityJsessionid1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJsessionid1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
