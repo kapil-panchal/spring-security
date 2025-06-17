a29ff35536e3a915575632935c942ceeda15f822
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2025-06-17 08:21:38 +0530

    [log] security debug logs

--- spring-security-jsessionid-1/src/main/java/com/java/session/SpringSecurityJsessionid1Application.java
+++ spring-security-jsessionid-1/src/main/java/com/java/session/SpringSecurityJsessionid1Application.java
@@ -2,8 +2,10 @@ package com.java.session;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class SpringSecurityJsessionid1Application {
 
 	public static void main(String[] args) {
--- spring-security-jsessionid-1/src/main/java/com/java/session/config/SecurityConfig.java
+++ spring-security-jsessionid-1/src/main/java/com/java/session/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.java.session;
+package com.java.session.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
